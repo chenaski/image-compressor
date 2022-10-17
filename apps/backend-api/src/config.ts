e314d24047f9a8230602262ab6e2fe668d760f59
chenaski <mail2chenaski@gmail.com>
2022-10-17 13:17:39 +0300

    [backend-api]: Use dynamic import to load dotenv package

---
+++ apps/backend-api/src/config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+
+let isEnvVarsLoaded = false;
+
+const loadEnvVars = async () => {
+  if (isEnvVarsLoaded) return;
+
+  isEnvVarsLoaded = true;
+
+  await import('dotenv')
+    .then((dotenv) => {
+      console.log('Load environment variables from `.env` and `.env.local` files');
+
+      dotenv.config();
+      dotenv.config({ path: path.resolve(__dirname, '../.env.local'), override: true });
+    })
+    .catch(() => {
+      console.log('Skip loading dotenv files');
+    });
+};
+
+export const getConfig = async () => {
+  await loadEnvVars();
+
+  const env = process.env;
+
+  const port = env['PORT'] || '4000';
+
+  return {
+    port: parseInt(port),
+    redisUrl: env['REDIS_URL'],
+  };
+};
--- apps/backend-api/src/index.ts
+++ apps/backend-api/src/index.ts
@@ -1,32 +1,34 @@
 import fastify from 'fastify';
 import websocket from '@fastify/websocket';
 import redis from '@fastify/redis';
-import dotenv from 'dotenv';
-import path from 'path';
 
 import { compressRoutes } from './routes/compress';
 import { pingRoutes } from './routes/ping';
 import { wsRoutes } from './routes/ws';
+import { getConfig } from './config';
 
-dotenv.config();
-dotenv.config({ path: path.resolve(__dirname, '../.env.local'), override: true });
+async function startServer() {
+  const server = fastify();
+  const config = await getConfig();
 
-const server = fastify();
+  server.register(websocket);
+  server.register(redis, {
+    url: process.env.REDIS_URL || undefined,
+  });
 
-server.register(websocket);
-server.register(redis, {
-  url: process.env.REDIS_URL || undefined,
-});
+  server.register(pingRoutes);
+  server.register(compressRoutes);
+  server.register(wsRoutes);
 
-server.register(pingRoutes);
-server.register(compressRoutes);
-server.register(wsRoutes);
+  server.listen({ port: config.port, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
-server.listen({ port, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+(async () => {
+  await startServer();
+})();
