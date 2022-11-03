527285e9ae078970362e87ecc155ba2b219ffa65
Eugene Boruhov <eugene.boruhov@gmail.com>
2022-11-03 18:47:16 +0300

    [website] Setup `prettier-plugin-tailwindcss`

--- apps/website/app/components/options/options-range.tsx
+++ apps/website/app/components/options/options-range.tsx
@@ -15,7 +15,7 @@ export const OptionsRange: React.FC<OptionsRangeProps> = ({ label, className }) 
         <span>{label}</span>
         <input
           className={
-            'border border-gray-400 w-[34px] bg-transparent text-center appearance-none transition hover:border-gray-500 focus:outline-none focus:border-gray-500 focus:ring focus:ring-1 focus:ring-gray-500'
+            'w-[34px] appearance-none  border border-gray-400 bg-transparent text-center transition hover:border-gray-500 focus:border-gray-500 focus:outline-none focus:ring focus:ring-1 focus:ring-gray-500'
           }
           type="text"
           inputMode={'numeric'}
@@ -23,10 +23,10 @@ export const OptionsRange: React.FC<OptionsRangeProps> = ({ label, className }) 
           value={quality}
         />
       </label>
-      <label className={'block mt-2'}>
+      <label className={'mt-2 block'}>
         <span className="sr-only">{label}</span>
         <input
-          className={'w-full range'}
+          className={'range w-full'}
           style={{
             '--range-fill-percent': `${quality}%`,
           }}
