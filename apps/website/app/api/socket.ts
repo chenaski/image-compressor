fc77184689329c8accb98e1d02aafcf74afac29a
Eugene Boruhov <eugene.boruhov@gmail.com>
2022-11-05 00:23:25 +0300

    [website] Use `zod` instead of manual parsing

--- apps/website/app/api/socket.ts
+++ apps/website/app/api/socket.ts
@@ -1,10 +1,6 @@
 import { configClient } from '~/config.client';
 import { useImages } from '~/stores/images';
-
-interface ImageData {
-  fileName: string;
-}
-type Message = ImageData[];
+import { parseMessage } from '~/utils/parse-message';
 
 let ws: WebSocket | null = null;
 
@@ -18,15 +14,17 @@ export async function connect({ isReconnect }: { isReconnect?: boolean } = { isR
   ws.addEventListener('open', () => console.log(`[WS] Connected ${url}`));
   ws.addEventListener('close', () => {
     console.log(`[WS] Closed ${url}`);
+    // TODO: limit retries
     connect({ isReconnect: true });
   });
   ws.addEventListener('error', (event) => console.log(`[WS] Error ${url}`));
   ws.addEventListener('message', (event) => {
-    const parsedData = parseMessage(event.data);
+    const result = parseMessage(event.data);
 
-    if (!parsedData) return;
+    // TODO: handle error
+    if (!result.success) return;
 
-    useImages.getState().setProcessedImages(parsedData);
+    useImages.getState().setProcessedImages(result.data);
   });
 }
 
@@ -36,36 +34,4 @@ export async function send(type: string, data: Record<string, unknown> | unknown
   }
 
   ws.send(JSON.stringify(data));
-}
-
-function parseMessage(message: unknown): Message | null {
-  if (!message || typeof message !== 'string') return null;
-
-  let parsedData: unknown;
-
-  try {
-    parsedData = JSON.parse(message);
-  } catch (error) {
-    return null;
-  }
-
-  const isNonEmptyArray = (value: unknown): value is unknown[] => {
-    return Array.isArray(value) && value.length > 0;
-  };
-  const isObject = (value: unknown): value is Record<string, unknown> => {
-    return typeof parsedData === 'object';
-  };
-  const isNonEmptyString = (value: unknown): value is string => {
-    return !(!value || typeof value !== 'string');
-  };
-  const isValidMessage = (value: unknown[]): value is Message => {
-    return value.every((item: unknown) => {
-      if (!isObject(item)) return false;
-      return isNonEmptyString(item.fileName);
-    });
-  };
-
-  if (!isNonEmptyArray(parsedData) || !isValidMessage(parsedData)) return null;
-
-  return parsedData;
 }
