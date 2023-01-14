a5187ffa56c2e64f25b886a5489f04119d1c6584
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-01-14 15:08:08 +0300

    [website] Add `SelectImagePanel`

---
+++ apps/website/app/components/select-image-panel.tsx
@@ -0,0 +1,41 @@
+import { Spinner } from '~/components/spinner';
+import type { ImagesState } from '~/stores/images';
+import { useImages } from '~/stores/images';
+
+export interface ImageSelectPanelProps {
+  images: ImagesState['images'];
+  selectedImageId: string;
+  setSelectedImageId: (id: string) => void;
+}
+
+export const SelectImagePanel = ({ selectedImageId, setSelectedImageId }: ImageSelectPanelProps) => {
+  const { images } = useImages();
+
+  return (
+    <div className={'mt-4 flex shrink-0 gap-2 overflow-x-auto'}>
+      {Object.entries(images).map(([id, { source, processed }]) => {
+        return (
+          <button
+            key={id}
+            className={`group relative h-[62px] w-[62px] overflow-hidden rounded border border-gray-200 bg-gray-200 transition hover:border-gray-400 ${
+              id === selectedImageId ? 'border-2 border-black' : ''
+            }`}
+            onClick={(e) => {
+              e.stopPropagation();
+              setSelectedImageId(id);
+            }}
+          >
+            <img
+              className={`h-full w-full object-cover transition group-hover:scale-110 group-hover:opacity-100 ${
+                id === selectedImageId ? '' : 'opacity-80'
+              } ${!processed ? 'blur-[1px]' : ''}`}
+              src={source.url}
+              alt=""
+            />
+            {!processed && <Spinner className={'absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'} />}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
--- apps/website/app/components/uploaded-screen.tsx
+++ apps/website/app/components/uploaded-screen.tsx
@@ -5,7 +5,7 @@ import { CloseIcon } from '~/components/icons/close-icon';
 import { DividerWithRef } from '~/components/image-comparison-slider/divider';
 import { ImagesContainerWithRef } from '~/components/image-comparison-slider/images-container';
 import { Options } from '~/components/options/options';
-import { Spinner } from '~/components/spinner';
+import { SelectImagePanel } from '~/components/select-image-panel';
 import { useComparisonSlider } from '~/hooks/use-comparison-slider';
 import { useDraggable } from '~/hooks/use-draggable';
 import { useImages } from '~/stores/images';
@@ -58,33 +58,7 @@ export const UploadedScreen = () => {
           }}
         />
 
-        <div className={'mt-4 flex shrink-0 gap-2 overflow-x-auto'}>
-          {Object.entries(images).map(([id, { source, processed }]) => {
-            return (
-              <button
-                key={id}
-                className={`group relative h-[62px] w-[62px] overflow-hidden rounded border border-gray-200 bg-gray-200 transition hover:border-gray-400 ${
-                  id === selectedImageId ? 'border-2 border-black' : ''
-                }`}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setSelectedImageId(id);
-                }}
-              >
-                <img
-                  className={`h-full w-full object-cover transition group-hover:scale-110 group-hover:opacity-100 ${
-                    id === selectedImageId ? '' : 'opacity-80'
-                  } ${!processed ? 'blur-[1px]' : ''}`}
-                  src={source.url}
-                  alt=""
-                />
-                {!processed && (
-                  <Spinner className={'absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'} />
-                )}
-              </button>
-            );
-          })}
-        </div>
+        <SelectImagePanel images={images} selectedImageId={selectedImageId} setSelectedImageId={setSelectedImageId} />
       </div>
 
       <Options className={'relative z-10 w-[420px] shrink-0'} />
