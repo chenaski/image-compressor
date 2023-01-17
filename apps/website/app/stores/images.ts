30eb19b2e7b30c3f93210118a1cd346b14e748a0
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-01-17 23:10:32 +0300

    [website] Use new `zustand` api

--- apps/website/app/stores/images.ts
+++ apps/website/app/stores/images.ts
@@ -1,5 +1,5 @@
 import produce from 'immer';
-import create from 'zustand';
+import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
 import { configClient } from '~/config.client';
