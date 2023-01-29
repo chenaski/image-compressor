bc505084914819f4363aa65d29609b1ee1f4a9b6
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-01-29 13:32:52 +0300

    [website] Replace `CloseIcon`

--- apps/website/app/components/icons/close-icon.tsx
+++ apps/website/app/components/icons/close-icon.tsx
@@ -1,8 +1,9 @@
-export const CloseIcon = () => {
+import type { FC } from 'react';
+
+export const CloseIcon: FC<{ className?: string }> = ({ className }) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 6L6 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M6 6L18 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <svg width="20" height="20" fill="#000" className={className}>
+      <path d="m17.072 4.34-5.657 5.656 5.656 5.657-1.414 1.414L10 11.41l-5.656 5.657-1.415-1.414 5.657-5.657L2.93 4.34l1.415-1.414L10 8.582l5.657-5.657 1.415 1.414Z" />
     </svg>
   );
 };
