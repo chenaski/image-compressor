b9fa619b40feb3af7890160a265fbd12405c1648
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-01-29 19:31:41 +0300

    [website] Change `Divider` styles

--- apps/website/app/components/image-comparison-slider/divider.tsx
+++ apps/website/app/components/image-comparison-slider/divider.tsx
@@ -1,15 +1,19 @@
 import type { ForwardedRef } from 'react';
 import { forwardRef } from 'react';
 
+import { ArrowsIcon } from '~/components/icons/arrows-icon';
+
 export const Divider = (props: unknown, ref: ForwardedRef<HTMLButtonElement>) => {
   return (
     <button className={'fixed top-0 left-[50%] bottom-0 cursor-col-resize px-[20px]'} ref={ref}>
-      <span className={'block h-full w-[4px] border-x border-white bg-gray-500'}></span>
+      <span className={'block h-full w-[3px] bg-white shadow-[0px_1px_1px_rgba(0,0,0,0.42)]'}></span>
       <span
         className={
-          'absolute top-[50%] left-[50%] h-[20px] w-[20px] translate-x-[-50%] translate-y-[-50%] rounded-full bg-gray-500'
+          'absolute top-[50%] left-[50%] flex h-[53px] w-[53px] translate-x-[-50%] translate-y-[-50%] items-center justify-center rounded-full bg-white shadow-button'
         }
-      ></span>
+      >
+        <ArrowsIcon />
+      </span>
     </button>
   );
 };
