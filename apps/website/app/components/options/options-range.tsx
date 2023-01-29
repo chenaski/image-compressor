9423fdeb3402be61053f10886de4599003105ed0
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-01-29 20:24:26 +0300

    [website] Change `OptionsRange` styles

--- apps/website/app/components/options/options-range.tsx
+++ apps/website/app/components/options/options-range.tsx
@@ -1,4 +1,5 @@
-import type { ChangeEvent } from 'react';
+import cn from 'classnames';
+import type { ChangeEvent, FC } from 'react';
 
 import { useOptions } from '~/stores/options';
 
@@ -6,25 +7,16 @@ export interface OptionsRangeProps {
   label: string;
   className?: string;
 }
-export const OptionsRange: React.FC<OptionsRangeProps> = ({ label, className }) => {
+
+export const OptionsRange: FC<OptionsRangeProps> = ({ label, className }) => {
   const quality = useOptions((state) => state.quality);
   const setQuality = useOptions((state) => state.setQuality);
 
   return (
-    <div className={className}>
-      <label className={'flex justify-between'}>
-        <span>{label}</span>
-        <input
-          className={
-            'w-[34px] appearance-none  border border-gray-400 bg-transparent text-center transition hover:border-gray-500 focus:border-gray-500 focus:outline-none focus:ring focus:ring-1 focus:ring-gray-500'
-          }
-          type="text"
-          inputMode={'numeric'}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => setQuality(+e.target.value)}
-          value={quality}
-        />
-      </label>
-      <label className={'mt-2 block'}>
+    <div className={cn(`flex items-center text-sm`, className)}>
+      <span>{label}</span>
+
+      <label className={'mx-[14px] flex grow items-center'}>
         <span className="sr-only">{label}</span>
         <input
           className={'range w-full'}
@@ -36,6 +28,18 @@ export const OptionsRange: React.FC<OptionsRangeProps> = ({ label, className }) 
           max={100}
           value={quality}
           onChange={(e: ChangeEvent<HTMLInputElement>) => setQuality(+e.target.value)}
+        />
+      </label>
+
+      <label>
+        <input
+          className={
+            'w-[18px] appearance-none border-b border-dashed border-[#007AFF] bg-transparent text-center transition focus:outline-0'
+          }
+          type="text"
+          inputMode={'numeric'}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setQuality(+e.target.value)}
+          value={quality}
         />
       </label>
     </div>
