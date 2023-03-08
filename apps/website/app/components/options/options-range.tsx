e92015bd112eaae2bdc306d3da7c65b2e122ec08
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-03-08 11:15:50 +0300

    [website] Fix `OptionsRange` with custom min/max values

--- apps/website/app/components/options/options-range.tsx
+++ apps/website/app/components/options/options-range.tsx
@@ -1,18 +1,21 @@
 import cn from 'classnames';
 import type { FC, HTMLProps } from 'react';
 
-export const OptionsRange: FC<Omit<HTMLProps<HTMLInputElement>, 'value'> & { value: number }> = ({
-  value,
-  className,
-  ...props
-}) => {
+export const OptionsRange: FC<
+  Omit<HTMLProps<HTMLInputElement>, 'value' | 'min' | 'max'> & { value: number; min?: number; max?: number }
+> = ({ value, className, min = 0, max = 100, ...props }) => {
+  const percent = (value / max) * 100;
+
   return (
     <input
       type="range"
       className={cn('range', className)}
       style={{
-        '--range-fill-percent': `${value}%`,
+        '--range-fill-percent': `${percent}%`,
       }}
+      value={value}
+      min={min}
+      max={max}
       {...props}
     />
   );
