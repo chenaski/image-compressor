3ead602f02295d6e7182c104de61b69c5a41c686
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-03-11 18:16:40 +0300

    [website] Add `CloseRoundedIcon`

---
+++ apps/website/app/components/icons/close-rounded-icon.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CloseRoundedIcon } from '~/components/icons/close-rounded-icon';
+
+export default {
+  title: 'Components/Icons/CloseRoundedIcon',
+  component: CloseRoundedIcon,
+} satisfies Meta<typeof CloseRoundedIcon>;
+
+export const Default: StoryObj<typeof CloseRoundedIcon> = {};
