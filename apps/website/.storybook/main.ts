0a1000a3b1cdc2d38dc6f58625dbe8ae1935b00b
Eugene Boruhov <eugene.boruhov@gmail.com>
2023-04-15 22:41:42 +0200

    [website] Set `colors` story as default

--- apps/website/.storybook/main.ts
+++ apps/website/.storybook/main.ts
@@ -3,7 +3,7 @@ import { mergeConfig } from 'vite';
 import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../app/components/**/*.stories.@(mdx|tsx)', '../stories/**/*.@(mdx|tsx)'],
+  stories: ['../stories/colors.mdx', '../app/components/**/*.stories.@(mdx|tsx)', '../stories/**/*.@(mdx|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
